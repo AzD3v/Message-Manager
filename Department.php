56da09831839888e57c5e3a4fc7ef250dd31303e
Cactusforlife <miguelsardinhapimentel@hotmail.com>
2018-03-26 21:30:10 +0000

    adição dos documentos exportados pelo UML

---
+++ Department.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * TODO Auto-generated comment.
+ */
+class Department {
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $NumDepartment;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $IdDepartment;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $department;
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function __construct($department) {
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getDepartment() {
+		return "";
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function setDepartment($department) {
+		return "";
+	}
+}
---
+++ message.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * TODO Auto-generated comment.
+ */
+class message {
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $numMessage;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $to;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $from;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $subject;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $text;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $date_hour;
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	private $attachment;
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function __construct($to, $from, $subject, $text, $date_hour) {
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getTo() {
+		return null;
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getFrom() {
+		return null;
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getSubject() {
+		return "";
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getText() {
+		return "";
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getDate_hour() {
+		return null;
+	}
+
+	/**
+	 * TODO Auto-generated comment.
+	 */
+	public function getAttachment() {
+		return "";
+	}
+}
