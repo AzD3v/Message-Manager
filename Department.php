ccafee5909e7643af95471832aee902a7a28f589
Cactusforlife <37628298+Cactusforlife@users.noreply.github.com>
2018-03-27 21:24:35 +0000

    Department v3

--- Department.php
+++ Department.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * TODO Auto-generated comment.
- */
+
 class Department {
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	Static $IdDepartment = 0;
 	static $nDepartments = 0;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $name;
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	public function __construct($name) {
 		
 		$this->id = Department::$IdDepartment++;
@@ -23,19 +15,17 @@ class Department {
 		++Department::$nDepartments;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	public function getDepartment() {
 		
 		return $this->name;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	public function setDepartment($name) {
 		
 		return $this->name = $name;
 	}
 }
+
+?>
