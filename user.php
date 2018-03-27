4360d55471d4fc9e2d7deab7b923d6caceee40ec
Cactusforlife <37628298+Cactusforlife@users.noreply.github.com>
2018-03-27 21:39:19 +0000

    User v2

--- user.php
+++ user.php
@@ -1,67 +1,38 @@
 <?php
-/**
- * TODO Auto-generated comment.
- */
  
 require 'Department.php';
 require 'message.php';
  
 class User {
-	/**
-	 * TODO Auto-generated comment.
-	 */
-	Static $NumUserID = 0;
-	/**
-	 * TODO Auto-generated comment.
-	 */
-	private $UserID;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
+	Static $IdUser = 0;
+
+	Static $nUsers = 0;
+	
 	private $idDepartment;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $name;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $surname;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $middle_name;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $username;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $password;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $cargo;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $contactList;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $inbox;
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	private $outbox;
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	public function __construct($name, $surname, $username, $password) {
 		
-		$this->id = User::$NumUserID++;
+		$this->id = User::$IdUser++;
 		$this->idDepartment = array();
 		$this->name = $name;
 		$this->surname = $surname;
@@ -72,11 +43,10 @@ class User {
 		$this->contactList = array();
 		$this->inbox = array();
 		$this->outbox = array();
+		++User::$nUsers;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
+	
 	public function getName() {
 		
 		return $this->name;
@@ -99,51 +69,35 @@ class User {
 		return $fullname;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function getUsername() {
 		
 		return $this->username;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function getCargo() {
 		
 		return $this->cargo;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function getContactList() {
 		
 		return $this->contactList;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function getInbox() {
 		
 		return $this->inbox;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function getOutbox() {
+		
 		return $this->outbox;
 	}
 
-	/**
-	 * TODO Auto-generated comment.
-	 */
 	public function setPassword($password) {
 		
 		return $this->password = $password;
 		
 	}
 }
+?>
